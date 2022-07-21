8756fc30614a5e812b0e3500c26974dd7d11a4c4
Ouasfi <amine1ouasfi@hotmail.fr>
2022-07-21 19:05:35 +0200

    add utils

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+def create_grid_points_from_bounds(minimun, maximum, res):
+    x = np.linspace(minimun, maximum, res)
+    X, Y, Z = np.meshgrid(x, x, x, indexing='ij')
+    X = X.reshape((np.prod(X.shape),))
+    Y = Y.reshape((np.prod(Y.shape),))
+    Z = Z.reshape((np.prod(Z.shape),))
+
+    points_list = np.column_stack((X, Y, Z))
+    del X, Y, Z, x
+    return points_list
