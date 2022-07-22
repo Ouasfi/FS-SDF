2381881cfa8ee1afdeaec06f1a7d8dfe3670c244
Ouasfi <amine1ouasfi@hotmail.fr>
2022-07-22 04:07:08 +0200

    complete gen utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 def create_grid_points_from_bounds(minimun, maximum, res):
     x = np.linspace(minimun, maximum, res)
     X, Y, Z = np.meshgrid(x, x, x, indexing='ij')
@@ -9,3 +10,9 @@ def create_grid_points_from_bounds(minimun, maximum, res):
     points_list = np.column_stack((X, Y, Z))
     del X, Y, Z, x
     return points_list
+def get_levelset(batch, dataset):
+    dataset = dataset.dataset if isinstance(dataset, torch.utils.data.dataset.Subset) else dataset
+    paths = batch.get('path')
+    p = [torch.from_numpy(dataset.get_level_set(path)).float() for path in paths]
+    p = torch.stack(p)
+    return (p, torch.zeros(p.shape[0], p.shape[1]))
